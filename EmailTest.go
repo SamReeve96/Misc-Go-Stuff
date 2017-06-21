a6bb625b666808d325c8724f7dfa3d590a679f65
Samuel Reeve <sreeve96@gmail.com>
2017-06-21 19:42:27 +0100

    Working on test file

--- EmailTest.go
+++ EmailTest.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"strings"
 	"testing"
 )
 
-//TestEmailVal...
 func TestEmailVal(t *testing.T) {
-
+	const s, sep, want = "chicken", "ken", 4
+	got := strings.Index(s, sep)
+	if got != want {
+		t.Errorf("Index(%q,%q) = %v; want %v", s, sep, want, got)
+	}
 }
