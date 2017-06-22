af3b0fbaacf8e8683a7c0784a33bbd726357c0d6
Samuel Reeve <sreeve96@gmail.com>
2017-06-22 14:14:46 +0100

    Corrected an incorrect exp results

--- EmailValidator/EmailValidator_test.go
+++ EmailValidator/EmailValidator_test.go
@@ -13,7 +13,7 @@ func TestEmailVal(t *testing.T) {
 		{"sreeve96@gmail.com", true},
 		{"lorum ipsum", false},
 		{"accountxboxsams@hotmail.com", true},
-		{"sr@eve96@$£%$^.com", true},
+		{"sr@eve96@$£%$^.com", false},
 	}
 
 	for _, c := range cases {
