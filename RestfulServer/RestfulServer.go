4a621e9be46da60790425441e195eab92bb3eb2c
Samuel Reeve <sreeve96@gmail.com>
2017-06-25 18:52:23 +0100

    Moved files into related folders, created new restful server file

---
+++ RestfulServer/RestfulServer.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+//import(
+//"encoding/json"
+//"fmt"
+//"net/http"
+//	)
+
+type Payload struct {
+	Stuff Data
+}
+
+type Data struct {
+	Fruit Fruits
+	Veg   Vegetables
+}
+
+type Fruits map[string]int
+type Vegetables map[string]int
+
+func serverRest(w http.ResponseWriter, r *http.Request) {
+	responce, err := getJSONResponce()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Fprintf(w, string(responce))
+}
+
+func main() {
+	http.HandleFunc("/", serverRest)
+	http.ListenAndServe("localhost:9001", nil)
+	fmt.Println("local host should be running on localhost:9001")
+}
+
+func getJSONResponce() ([]byte, error) {
+	fruits := make(map[string]int)
+	fruits["Apples"] = 25
+	fruits["Oranges"] = 11
+
+	Vegetables := make(map[string]int)
+	Vegetables["Carrots"] = 21
+	Vegetables["Peppers"] = 0
+
+	d := Data{fruits, Vegetables}
+	p := Payload{d}
+
+	return json.MarshalIndent(p, "", "  ")
+}
